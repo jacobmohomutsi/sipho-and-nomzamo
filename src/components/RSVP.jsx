846046434c89ec61b7d50b7e8fefe68506b4e85d
Jacob Mohomutsi <jacobmohomutsi@Jacobs-MacBook-Pro.local>
2025-04-26 12:12:30 +0200

    updates

--- src/components/RSVP.jsx
+++ src/components/RSVP.jsx
@@ -8,7 +8,7 @@ const RSVP = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    const message = `Hello Thotho, I am ${name} and ${attendance === 'yes' ? 'I will attend' : 'I won\'t be able to attend'}. I hope you understand. ${note ? 'Note: ' + note : ''}`
+    const message = `Hello Orapeleng, I am ${name} and ${attendance === 'yes' ? 'I will attend' : 'I won\'t be able to attend'}. I hope you understand. ${note ? 'Note: ' + note : ''}`
     const encodedMessage = encodeURIComponent(message)
     const phoneNumber = '27637714375' // Replace with actual number
     const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodedMessage}`
@@ -29,8 +29,8 @@ const RSVP = () => {
           <div className="col-md-6 d-flex flex-column justify-content-center">
             <h2>RSVP & Info</h2>
             <p><strong>When:</strong> Saturday, July 26, 2025 · 10:00 AM - 16:30 PM</p>
-            <p><strong>Ceremony:</strong> UCKG, Mabopane, Unit B, 0190</p>
-            <p><strong>Reception:</strong> Mystical Woods Venue, No 100, Old Brits Road, Pretoria, 0251</p>
+            <p><strong>Ceremony:</strong> UCKG, Mabopane, Unit B, 0190 <a className="fw-semibold text-alpha text-decoration-underline" href="https://maps.app.goo.gl/fHzT94oqVRLoLXwH8" target="_blank">Get Location</a></p>
+            <p><strong>Reception:</strong> Mystical Woods Venue, No 100, Old Brits Road, Pretoria, 0251 <a className="fw-semibold text-alpha text-decoration-underline" href="https://maps.app.goo.gl/JmtwvdSx5RohPSwE7" target="_blank">Get Location</a></p>
             <p><strong>Dress Code:</strong> Elegant Attire</p>
             <form onSubmit={handleSubmit}>
               <input
