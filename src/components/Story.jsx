83104731311aa1b9f4a8647005a0abb7114c2fe1
Jacob Mohomutsi <jacobmohomutsi@Jacobs-MacBook-Pro.local>
2025-04-26 09:08:01 +0200

    updates

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -9,7 +9,7 @@ const Story = () => (
         WebkitOverflowScrolling: 'touch'
       }}
     >
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,].map((num, index) => (
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,].map((num, index) => (
         <div
           key={index}
           className="flex-shrink-0 mx-2"
